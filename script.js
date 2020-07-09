83302ad18209bf3282d9c4749e44c4a6d650a9b4
Donovan Dixon <dddixon00@gmail.com>
2020-07-08 22:15:08 -0400

    Addition of Sign up page, various updates.

--- script.js
+++ script.js
@@ -27,3 +27,28 @@ window.addEventListener('scroll', function(e) {
 
   new_scroll_position = last_scroll_position;
 });
+
+function validateForm() {
+  if (isEmpty(document.getElementById('data_2').value.trim())) {
+  alert('First name is required!');
+  return false;
+  }
+  if (isEmpty(document.getElementById('data_3').value.trim())) {
+  alert('Last name is required!');
+  return false;
+  }
+  if (isEmpty(document.getElementById('data_4').value.trim())) {
+  alert('Email is required!');
+  return false;
+  }
+  if (!validateEmail(document.getElementById('data_4').value.trim())) {
+  alert('Email must be a valid email address!');
+  return false;
+  }
+  return true;
+  }
+  function isEmpty(str) { return (str.length === 0 || !str.trim()); }
+  function validateEmail(email) {
+  var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,15}(?:\.[a-z]{2})?)$/i;
+  return isEmpty(email) || re.test(email);
+  }
