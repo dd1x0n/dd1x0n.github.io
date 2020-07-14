3e90dbc4234956fd08a700826b7d9b13fcf5db39
Donovan Dixon <dddixon00@gmail.com>
2020-07-14 17:15:31 -0400

    -changes to media q's, nav bar, :nth child add.

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 // while (secret.toLowerCase() !== "analog"); 
 
 //Below is the JavaScript needed to have the nav bar pop in and out.
+
 var new_scroll_position = 0;
 var last_scroll_position;
 var header = document.getElementById("header");
@@ -18,7 +19,7 @@ window.addEventListener('scroll', function(e) {
   if (new_scroll_position < last_scroll_position && last_scroll_position > 80) { 
     header.classList.remove("slideDown");
     header.classList.add("slideUp");
-
+    
   // Scroll up
   } else if (new_scroll_position > last_scroll_position) {
     header.classList.remove("slideUp");
@@ -28,25 +29,32 @@ window.addEventListener('scroll', function(e) {
   new_scroll_position = last_scroll_position;
 });
 
+// JS for form validation req. 1/3
+
 function validateForm() {
   if (isEmpty(document.getElementById('data_2').value.trim())) {
   alert('First name is required!');
   return false;
   }
+
   if (isEmpty(document.getElementById('data_3').value.trim())) {
   alert('Last name is required!');
   return false;
   }
+
   if (isEmpty(document.getElementById('data_4').value.trim())) {
   alert('Email is required!');
   return false;
   }
+
   if (!validateEmail(document.getElementById('data_4').value.trim())) {
   alert('Email must be a valid email address!');
   return false;
   }
+
   return true;
   }
+
   function isEmpty(str) { return (str.length === 0 || !str.trim()); }
   function validateEmail(email) {
   var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,15}(?:\.[a-z]{2})?)$/i;
